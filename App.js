b704b2230f246689af6677c746824c85fefd5961
shrinesh <146428342+shrinesh16@users.noreply.github.com>
2025-03-14 18:20:33 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css"; // Keep Bootstrap Import
+import Home from "./Home"; // Import Home Component
+
+function App() {
+  return (
+    <div>
+      <Home /> {/* Renders Home Component */}
+    </div>
+  );
+}
+
+export default App;
